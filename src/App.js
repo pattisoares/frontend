cf226f28268331d60e0cbbe886c6462b73224c85
Patricia Soares <patfatec@gmail.com>
2026-06-30 18:25:32 -0300

    feat: adicionado controle de login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import './App.css';
+
+import Login from './pages/Login';
 import Recados from './pages/Recados';
 
 function App() {
-  return (
-    <div className="App">
-      <Recados />
-    </div>
-  );
+
+    const [logado, setLogado] = useState(
+        !!localStorage.getItem('token')
+    );
+
+    return (
+        <div className="App">
+
+            {logado ? (
+                <Recados />
+            ) : (
+                <Login onLogin={() => setLogado(true)} />
+            )}
+
+        </div>
+    );
 }
 
 export default App;
