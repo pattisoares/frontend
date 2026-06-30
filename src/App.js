05c74bc534a8f92301eac74d1108f61cff7d8393
Patricia Soares <patfatec@gmail.com>
2026-06-30 18:32:10 -0300

    feat: adicionando navegacao entre login cadastro e recados

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import { useState } from 'react';
 import './App.css';
 
 import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
 import Recados from './pages/Recados';
 
 function App() {
-
     const [logado, setLogado] = useState(
         !!localStorage.getItem('token')
     );
+
+    const [mostrarCadastro, setMostrarCadastro] = useState(false);
 
     return (
         <div className="App">
 
             {logado ? (
                 <Recados />
+            ) : mostrarCadastro ? (
+                <Cadastro
+                    onCancelar={() => setMostrarCadastro(false)}
+                />
             ) : (
-                <Login onLogin={() => setLogado(true)} />
+                <Login
+                    onLogin={() => setLogado(true)}
+                    onCadastrar={() => setMostrarCadastro(true)}
+                />
             )}
 
         </div>
